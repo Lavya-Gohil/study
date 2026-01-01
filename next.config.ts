6912ceb5797ad478c2ec39cadf329b170f7a15e9
Lavya Gohil <gohil.lavya@gmail.com>
2026-01-01 21:42:27 +0300

    Revert to Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/APP',
-  images: {
-    unoptimized: true,
-  },
+
   experimental: {
     optimizePackageImports: ['@mui/material', '@mui/icons-material', 'recharts'],
   },
